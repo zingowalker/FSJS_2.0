8994a9f1794d4151f15f377d84947022c2c12d4b
Tsetan Dorje <tsetangrace@gmail.com>
2022-12-21 16:43:34 +0530

    hero section complete

--- project8_HostingSite/main.js
+++ project8_HostingSite/main.js
@@ -1,13 +1,13 @@
 
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+// let menu = document.querySelector('#menu-bar');
+// let navbar = document.querySelector('.navbar');
 
-window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+// window.onscroll = () => {
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// }
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-});
+// menu.addEventListener('click', () => {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// });
