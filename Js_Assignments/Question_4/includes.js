056c066cb07af3b5b20f2ab5b2013452ddc8e293
Tsetan Dorje <tsetangrace@gmail.com>
2023-01-11 20:33:48 +0530

    js assignment till question 4

---
+++ Js_Assignments/Question_4/includes.js
@@ -0,0 +1,7 @@
+// 04. Declare a varibale and assign string value to
+// it and check if the string contains 
+// a word Script using includes() method.
+
+const js = "JavaScript is the world's most popular programming language"
+
+console.log(js.includes('Script')) 
