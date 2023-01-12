7b31f70e17561d8bc75a07d90e02faf7204f683d
Tsetan Dorje <tsetangrace@gmail.com>
2023-01-12 18:05:55 +0530

    js question 5 complete

---
+++ Js_Assignments/Question_5/split.js
@@ -0,0 +1,9 @@
+// Declare a variable and assign string value to it and 
+// then split it into an array using split() method
+
+
+const string = "the quick brow fox jump over the lazy dog";
+
+const splitString = string.split('')
+
+console.log(splitString)
