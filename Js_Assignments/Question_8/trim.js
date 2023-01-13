b25a95d3f167dc83930dd522a30466a30648e0de
Tsetan Dorje <tsetangrace@gmail.com>
2023-01-13 12:14:08 +0530

    deleted .DS_Store file and js question 8 complete

---
+++ Js_Assignments/Question_8/trim.js
@@ -0,0 +1,7 @@
+// 08. Demonstrate the use of trim() to remove any 
+// trailing whitespace at the beginning and 
+// the end of a string.
+
+const whitespace = '  remove whitespace  '
+console.log(whitespace)
+console.log(whitespace.trim()) // trim method removes white space from front and the back 
