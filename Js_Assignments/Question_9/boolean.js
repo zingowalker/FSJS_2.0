21c38145014e34a20661e1e7d1bf175e3ac05ef9
Tsetan Dorje <tsetangrace@gmail.com>
2023-01-14 21:30:03 +0530

    js assignment quetion 9 complete

---
+++ Js_Assignments/Question_9/boolean.js
@@ -0,0 +1,22 @@
+// 09. Boolean value is either true or false.
+//  - Write three JavaScript statement example which 
+// provide truthy value.
+//  - Write three JavaScript statement example which 
+// provide falsy value.
+
+const frontend = 'html, css and js';
+const fName = '1';
+const lName = 'true'
+
+const falsy1 = '';
+const falsy2 = 0;
+const falsy3 = undefined;
+
+// console.log(frontend ? 'true' : 'false')
+// if(fName){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// console.log(falsy3 ? 'true' : 'false');
