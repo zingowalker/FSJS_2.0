0eb5ea05b6f279f29b205dae1cdd4f0c8e4b0f30
Tsetan Dorje <tsetangrace@gmail.com>
2023-01-15 21:55:25 +0530

    js assignemt question 12 folder created

---
+++ Js_Assignments/Question_12/dateTime.js
@@ -0,0 +1,12 @@
+// 12. Create a human readable time format using the Date time object
+//     - YYYY-MM-DD HH:mm
+//     - DD-MM-YYYY HH:mm
+//     - DD/MM/YYYY HH:mm
+
+let now = new Date()
+
+let current = now.getFullYear(  )
+
+console.log(current)
+
+
