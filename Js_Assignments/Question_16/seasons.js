4d8ed4670d39de7f9fdd4d656351f27de933b340
Tsetan Dorje <tsetangrace@gmail.com>
2023-01-17 11:48:46 +0530

    js question 16 complete

---
+++ Js_Assignments/Question_16/seasons.js
@@ -0,0 +1,22 @@
+//16. Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+//    - September, October or November, the season is Autumn.
+//    - December, January or February, the season is Winter.
+//    - March, April or May, the season is Spring
+//    - June, July or August, the season is Summer
+
+const seasons = {
+    autumn: [9, 10, 11],
+    winter: [12, 1, 2],
+    spring: [3, 4, 5],
+    summer: [6, 7, 8]
+}
+
+function getCurrentSeason(){
+    let currentMonth = new Date().getMonth() + 1; // months index start from 0, hence + 1
+    for (let season in seasons){
+        if (seasons[season].includes(currentMonth)){
+            return season;
+        }
+    }
+}
+console.log(getCurrentSeason());
