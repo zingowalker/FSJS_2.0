459a976b8075da7f7f9cde97a24504c62f304a00
Tsetan Dorje <tsetangrace@gmail.com>
2023-02-01 18:36:47 +0530

    04 dom project complete

--- javaScript/domProjects/04_DOM Project/index.js
+++ javaScript/domProjects/04_DOM Project/index.js
@@ -1,26 +1,16 @@
-// color array
-// then we will use querySelector to select class from index.html file
-// once we have the class name, we can then manipulate the content 
-// for this project we will give color to all character selecting the class unit stats, 
-// querSelector for sats text to change to white color from dom
-
-
 
 const colors = [
-    '#F2921D', '#A61F69', '#FC7300', '#AACB73', '#8DCBE6'
+    '#ec9b3b', '#ee5487', '#f6901a', '#82bb30', '#4facff'
 ]
 
 const colorStats = document.querySelectorAll('.clash-card__unit-stats');
 const stat = document.querySelectorAll('.one-third')
 
-
-
-colorStats[0].style.backgroundColor = colors[0]
-colorStats[1].style.backgroundColor = colors[1]
-colorStats[2].style.backgroundColor = colors[2]
-colorStats[3].style.backgroundColor = colors[3]
-colorStats[4].style.backgroundColor = colors[4]
-
+for (i = 0; i < colorStats.length; i++) {
+    for (j = 0; j < colors.length; j++) {
+        colorStats[i].style.backgroundColor = colors[i]
+    }
+}
 for (i = 0; i < stat.length; i++) {
     stat[i].style.color = '#fff';
 }
