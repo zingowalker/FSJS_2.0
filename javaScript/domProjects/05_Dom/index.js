cc31d9856c58a2ccb982c1235c256279d3865308
Tsetan Dorje <tsetangrace@gmail.com>
2023-02-02 16:10:03 +0530

    dom project 5 done but need to make a tag cursor pointer

--- javaScript/domProjects/05_Dom/index.js
+++ javaScript/domProjects/05_Dom/index.js
@@ -1,27 +1,33 @@
 // part one
+
+
+
+const newDiv = document.createElement('div')
+document.querySelector('.recipe-gallery').appendChild(newDiv)
+
+const link = document.createElement('a')
+const image = document.createElement('img')
+image.src = './img/recipe-6.png'
+const h5 = document.createElement('h5')
+const para = document.createElement('p')
+
+
+h5.innerText = 'Chicken Biryani'
+para.innerText = 'Prep : 20min | Cook : 30min'
+
+
+newDiv.append(link)
+link.append(image, h5, para)
+
+
+newDiv.classList.add('card')
+link.className = ('recipe-text')
+image.classList.add('recipe-img')
+h5.classList.add('recipe-name')
+para.classList.add('recipe-disp')
+
 const recipeText = document.querySelectorAll(".recipe-text");
 
 recipeText.forEach((ele) => {
     ele.style.color = 'blueviolet'
 })
-
-const newDiv = document.createElement('div')
-document.querySelector('.recipe-gallery').appendChild(newDiv)
-
-const image = document.createElement('img')
-image.src = './img/recipe-6.png'
-newDiv.append(image)
-
-const link = document.createElement('a')
-
-
-
-
-const h5 = document.createElement('h5')
-const para = document.createElement('p')
-
-newDiv.className = 'card'
-link.className = 'recipe-text'
-image.className = 'recipe-img'
-h5.className = 'recipe-name'
-para.className = 'recipe-disp'
