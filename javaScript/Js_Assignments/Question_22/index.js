90bb6fbc14211a9711c30e52487c115be04b790b
Tsetan Dorje <tsetangrace@gmail.com>
2023-02-16 13:51:39 +0530

    added js assignment folde question 22 and half completed

---
+++ javaScript/Js_Assignments/Question_22/index.js
@@ -0,0 +1,24 @@
+// 22. The following is an array of 10 students ages:
+//     => const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+//     - Sort the array and find the min and max age
+//     - Find the median age(one middle item or two middle items divided by two)
+//     - Find the average age(all items divided by number of items)
+//     - Find the range of the ages(max minus min)
+//     - Compare the value of (min - average) and (max - average), use abs() method
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+
+// console.log(Math.min(...ages))
+// console.log(Math.max(...ages))
+
+
+// let max = ages[0], min = ages[0];
+
+// for (let i = 0; i < ages.length; i++) {
+//     if (ages[i] < min) { min = ages[i] }
+//     if (ages[i] > max) { max = ages[i] }
+// }
+// console.log(min)
+// console.log(max)
+
